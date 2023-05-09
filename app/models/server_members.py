d44f6bfa2c56bd04caefd175cafc81c7384467d9
valeriareynososf <valeriayearup@gmail.com>
2023-05-09 16:55:51 -0700

    server members table

--- app/models/server_members.py
+++ app/models/server_members.py
@@ -4,15 +4,15 @@ from sqlalchemy import UniqueConstraint
 class Server_Member(db.Model):
     __tablename__ = 'server_members'
     if environment == "production":
-        __table_args__ = ({'schema': SCHEMA})
+        __table_args__ = ({'schema': SCHEMA}, UniqueConstraint('user_id', 'server_id', name='server_join'))
+    else: __table_args__ = (UniqueConstraint('user_id', 'server_id', name='server_join'))
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
     server_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("servers.id")), nullable=False)
     user = db.relationship("User", back_populates="server_members")
     server = db.relationship("Server", back_populates="server_members_2")
-    __table_args__ = (UniqueConstraint('user_id', 'server_id', name='server_join'),
-                    )
+    # __table_args__ = (UniqueConstraint('user_id', 'server_id', name='server_join'))
 
     def to_dict(self):
         return {
