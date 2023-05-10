074957a020071856e6050c75237f09372577c693
valeriareynososf <valeriayearup@gmail.com>
2023-05-09 17:26:46 -0700

    updated seeders

--- app/seeds/server_members.py
+++ app/seeds/server_members.py
@@ -2,28 +2,26 @@ from app.models import db, User, Server, Server_Member
 import random
 
 def seed_members():
-    channel1message1 = Server_Member(server_id=1, user_id=1)
-    channel1message2 = Server_Member(server_id=2, user_id=2)
-    channel1message3 = Server_Member(server_id=3, user_id=3)
-    channel2message1 = Server_Member(server_id=4, user_id=3)
+    # channel1message1 = Server_Member(server_id=1, user_id=1)
+    # channel1message2 = Server_Member(server_id=2, user_id=2)
+    # channel1message3 = Server_Member(server_id=3, user_id=3)
+    # channel2message1 = Server_Member(server_id=4, user_id=3)
 
-    db.session.add(channel1message1)
-    db.session.add(channel1message2)
-    db.session.add(channel1message3)
-    db.session.add(channel2message1)
+    # db.session.add(channel1message1)
+    # db.session.add(channel1message2)
+    # db.session.add(channel1message3)
+    # db.session.add(channel2message1)
 
-
-
-    # nums = [num for num in range(1, 29)]
-    # for i in range(1, 29):
-    #     memberNums = []
-    #     for _ in range(10):
-    #         rand = random.choice(nums)
-    #         if rand not in memberNums:
-    #             memberNums.append(rand)
-    #     for j in memberNums:
-    #         if j != i:
-    #             db.session.add(Server_Member(server_id=i, user_id=j))
+    nums = [num for num in range(1, 29)]
+    for i in range(1, 29):
+        memberNums = []
+        for _ in range(10):
+            rand = random.choice(nums)
+            if rand not in memberNums:
+                memberNums.append(rand)
+        for j in memberNums:
+            if j != i:
+                db.session.add(Server_Member(server_id=i, user_id=j))
 
     db.session.commit()
 
